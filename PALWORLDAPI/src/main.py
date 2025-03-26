4667b95ebfe2d475f9d2b24b214a1b533682ab1a
Sadturtleman <rugsn12345@gmail.com>
2025-03-26 21:38:30 +0900

    change

--- PALWORLDAPI/src/main.py
+++ PALWORLDAPI/src/main.py
@@ -1,1 +1,13 @@
-print("test123asdasdff")
+# Description: This file contains the example functions that will be used in the API
+
+def add_numbers(a,b) : 
+    # This function adds two numbers
+    result = a + b
+    return result
+
+def divide(x, y) :
+    # This function divides two numbers
+    if y == 0:
+        print("Cannot divide by zero")
+    else:
+        return x / y
